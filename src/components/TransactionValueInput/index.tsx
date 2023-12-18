4541eeae8e5329d12a5ac573ed7f125d82426c9b
VinewZ <vinewalves@hotmail.com>
2023-12-18 12:12:02 -0300

    Style: increase input size based on its value

--- src/components/TransactionValueInput/index.tsx
+++ src/components/TransactionValueInput/index.tsx
@@ -1,19 +1,43 @@
 'use client'
 import { Input } from '@/components/_shadcn/ui/input'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 
 export function TransactionValueInput() {
   const [inputValue, setInputValue] = useState<undefined | number>()
+  const [inputWidth, setInputWidth] = useState('1ch')
+
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+    const value =
+      e.target.value.trim() !== '' ? Number(e.target.value) : undefined
+    const width =
+      value?.toString() === undefined
+        ? '1ch'
+        : value.toString().length + 1 + 'ch'
+
+    setInputValue(value)
+    setInputWidth(width)
+  }
 
   return (
-    <div className="mt-8 flex items-center justify-end border-b border-white/20 text-3xl font-bold">
-      <span className="text-white">$</span>
+    <div className="mt-8 flex items-center justify-end border-b border-white/20 pr-2 text-3xl font-bold">
+      <span
+        className={`${
+          inputValue?.toString() !== undefined &&
+          inputValue?.toString().length > 0
+            ? 'text-white'
+            : 'text-white/20'
+        } pr-2`}
+      >
+        $
+      </span>
       <Input
-        className="border-none text-3xl placeholder:text-white/20"
-        placeholder="0,00"
+        className="max-w-[90%] border-none p-0 text-right text-3xl placeholder:text-white/20"
+        placeholder="0"
         type="number"
         value={inputValue}
-        onChange={(e) => setInputValue(Number(e.target.value))}
+        onChange={(e) => handleInputChange(e)}
+        style={{ width: inputWidth }}
+        disabled
       />
     </div>
   )
